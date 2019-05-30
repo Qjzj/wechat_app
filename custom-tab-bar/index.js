8d3dfedf31764a3c4c7afc6745854b6d6bc64fec
Qjzj <915851703@qq.co>
2019-05-31 00:06:08 +0800

    tabBar

---
+++ custom-tab-bar/index.js
@@ -0,0 +1,42 @@
+Component({
+  data: {
+    selectedIndex: 0,
+    list: [{
+        "pagePath": "/pages/index/index",
+        "text": "首页",
+        "iconPath": "/assets/icon/home.png",
+        "selectedIconPath": "/assets/icon/home-select.png"
+      },
+      {
+        "pagePath": "/pages/logs/logs",
+        "text": "乘车",
+        "iconPath": "/assets/icon/code.png",
+        "selectedIconPath": "/assets/icon/code.png"
+      },
+      {
+        "pagePath": "/pages/mine/mine",
+        "text": "我的",
+        "iconPath": "/assets/icon/mine.png",
+        "selectedIconPath": "/assets/icon/mine-select.png"
+      }
+    ]
+  },
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset;
+      const url = data['path'];
+      const index = data['index']
+      this.setData({
+        selectedIndex: index
+      }, () => {
+        console.log(this.data.selectedIndex, 111);
+      })
+      console.log(index);
+      wx.switchTab({
+        url
+      });
+
+
+    }
+  }
+})
